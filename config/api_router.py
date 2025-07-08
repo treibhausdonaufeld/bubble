f14f36752d0d767396e507d2ece998246a755fcd
Fabian Helm <fabian@hoad.at>
2025-07-08 07:37:27 +0200

    cleanup categories api

--- config/api_router.py
+++ config/api_router.py
@@ -1,12 +1,14 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
+from bubble.categories.api.views import CategoryViewSet
 from bubble.items.api.views import ImageViewSet, ItemViewSet
 from bubble.users.api.views import UserViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register("users", UserViewSet)
+router.register("categories", CategoryViewSet, basename="category")
 router.register("items", ItemViewSet, basename="item")
 router.register("images", ImageViewSet, basename="image")
 
