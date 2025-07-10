ad00ae90685f5098397c07e6ace1c5b993490a6a
Fabian Helm <firlevapz@gmail.com>
2025-07-10 20:46:59 +0200

    remove core context processors

--- config/settings/base.py
+++ config/settings/base.py
@@ -209,9 +209,6 @@ TEMPLATES = [
                 "django.template.context_processors.tz",
                 "django.contrib.messages.context_processors.messages",
                 "bubble.users.context_processors.allauth_settings",
-                "bubble.core.context_processors.navigation_context",
-
-                "bubble.core.context_processors.theme_context",
             ],
         },
     },
