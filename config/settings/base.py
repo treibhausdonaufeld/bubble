4479e984089b8a097203c8d44e3d8ac6684f6872
Fabian Helm <firlevapz@gmail.com>
2025-10-03 19:56:36 +0200

    enable django forms again

--- config/settings/base.py
+++ config/settings/base.py
@@ -73,7 +73,7 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
     # "django.contrib.humanize", # Handy template tags
     "django.contrib.admin",  # required
-    # "django.forms",
+    "django.forms",
 ]
 THIRD_PARTY_APPS = [
     "guardian",
