80559a7ba50faae38d76e133a3e7de5e982ff4c9
Fabian Helm <firlevapz@gmail.com>
2025-10-19 18:41:04 +0200

    fix: close db connections upon websocket disconnect

--- backend/bubble/core/consumers.py
+++ backend/bubble/core/consumers.py
@@ -3,7 +3,9 @@
 import json
 import logging
 
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.db import close_old_connections
 
 logger = logging.getLogger(__name__)
 
@@ -58,6 +60,8 @@ class NotificationConsumer(AsyncWebsocketConsumer):
                 self.user.username,
                 close_code,
             )
+
+        await database_sync_to_async(close_old_connections)()
 
     async def receive(self, text_data=None, bytes_data=None):
         """Handle messages from WebSocket (optional ping/pong)."""
