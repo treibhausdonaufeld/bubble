720fec6b84e9ca7c5494af72edd8d57dde954a68
Fabian Helm <firlevapz@gmail.com>
2025-10-19 22:16:25 +0200

    increase crontab schedule

--- backend/bubble/bookings/beats.py
+++ backend/bubble/bookings/beats.py
@@ -8,7 +8,7 @@ app.conf.beat_schedule.update(
     {
         "bookings.check_active_10min": {
             "task": "bubble.bookings.tasks.check_bookings_active",
-            "schedule": crontab(minute="*/1"),
+            "schedule": crontab(minute="*/10"),
         }
     }
 )
